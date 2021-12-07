5cbec5da573a6a4d4fc3616e2eaf7192b66033f3
娄文霄 <86142265+louwenxiao@users.noreply.github.com>
2021-12-07 20:55:08 +0800

    Update models.py

--- data_and_model/models.py
+++ data_and_model/models.py
@@ -2,134 +2,265 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
-from abc import ABC
 
-# 定义三个模型，CNN，DNN，MCLR(多重逻辑回归）
 
-class CNN(nn.Module):
-    def __init__(self,in_dim,width,height,n_classes):   # 输入维度，宽度，高度，类别数
-        super(CNN,self).__init__()
-        self.width = width
-        self.height =height
+# -----------------------  MNIST  -------------------------
 
-        self.conv1 = nn.Sequential(        # 卷积1，输出维度8，卷积核为3,步长为1,填充1
-            nn.Conv2d(in_dim,8,3,1,1),
+class MNIST_CNN(nn.Module):
+    def __init__(self):
+        super(MNIST_CNN,self).__init__()
+
+        self.conv1 = nn.Sequential(        
+            nn.Conv2d(1,16,3,1,1),
             nn.ReLU(),
             nn.MaxPool2d(2,2)
         )
 
-        self.conv2 = nn.Sequential(        # 卷积2，输出维度16，卷积核为3,步长为1，填充1
-            nn.Conv2d(8,16,3,1,1),
-            nn.ReLU(),
-            nn.AvgPool2d(2,2)
-        )
-
-        self.fc1 = nn.Linear(((width//2)//2)*((height//2)//2)*16,128)
-        self.fc2 = nn.Linear(128, 84)
-        self.fc3 = nn.Linear(84,n_classes)
-
-    def forward(self,x):
-        out_conv1 = self.conv1(x)
-        out_conv2 = self.conv2(out_conv1)
-        output = out_conv2.view(-1,((self.width//2)//2)*((self.height//2)//2)*16)
-        output = F.relu(self.fc1(output))
-        output = F.relu(self.fc2(output))
-        output = self.fc3(output)
-        output = F.log_softmax(output,dim=1)
-        return output
-
-
-class DNN(nn.Module):
-    def __init__(self,in_dim,width,height,n_classes,mid_dim=128):  # 输入维度，宽度，高度，类别数,中间维度默认128
-        super(DNN,self).__init__()
-
-        self.in_dim =in_dim
-        self.width = width
-        self.height =height
-
-        self.fc1 = nn.Linear(in_dim * width * height,mid_dim)
-        self.fc2 = nn.Linear(128, n_classes)
-
-    def forward(self,x):
-        x = x.view(-1,self.in_dim*self.height*self.width)
-        output = self.fc1(x)
-        x = F.relu(output)
-        output = self.fc2(x)
-        output = F.log_softmax(output,dim=1)
-        return output
-
-
-class MCLR_Logistic(nn.Module):
-    def __init__(self, in_dim,width,height,n_classes):   # 输入维度，宽度，高度，类别数
-        super(MCLR_Logistic, self).__init__()
-        self.in_dim =in_dim
-        self.width = width
-        self.height =height
-        self.fc1 = nn.Linear(in_dim * width * height,n_classes)
-
-    def forward(self, x):
-        x = x.view(-1,self.in_dim*self.height*self.width)
-        x = self.fc1(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-
-
-class CNNCIFAR(nn.Module):
-    def __init__(self,in_dim,width,height,n_classes):   # 输入维度，宽度，高度，类别数
-        super(CNNCIFAR,self).__init__()
-        self.width = width
-        self.height =height
-
-        self.conv1 = nn.Sequential(        # 卷积1，输出维度6，卷积核为3,步长为1,填充1
-            nn.Conv2d(in_dim,16,3,1,1),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2)
-        )
-
-        self.conv2 = nn.Sequential(        # 卷积2，输出维度32，卷积核为3,步长为1，填充1
+        self.conv2 = nn.Sequential(     
             nn.Conv2d(16,32,3,1,1),
             nn.ReLU(),
-            nn.AvgPool2d(2,2)
-        )
-        self.conv3 = nn.Sequential(        # 卷积2，输出维度128，卷积核为3,步长为1，填充1
-            nn.Conv2d(32,128,3,1,1),
-            nn.ReLU(),
-            nn.AvgPool2d(2,2)
+            nn.MaxPool2d(2,2)
         )
 
-        self.fc1 = nn.Linear(4*4*128,256)
-        self.fc2 = nn.Linear(256, 84)
-        self.fc3 = nn.Linear(84,n_classes)
+        self.fc1 = nn.Linear(7*7*32,128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self,x):
         out_conv1 = self.conv1(x)
         out_conv2 = self.conv2(out_conv1)
-        out_conv3 = self.conv3(out_conv2)
-        output = out_conv3.view(-1,16*128)
+        output = out_conv2.view(-1,7*7*32)
         output = F.relu(self.fc1(output))
-        output = F.relu(self.fc2(output))
-        output = self.fc3(output)
-        output = F.log_softmax(output,dim=1)
-        return output
+        output = self.fc2(output)
+        return F.log_softmax(output,dim=1)
+
+class MNIST_LR_Net(nn.Module):
+    def __init__(self):
+        super(MNIST_LR_Net, self).__init__()
+
+        self.hidden1 = nn.Linear(28 * 28, 512)
+        self.hidden2 = nn.Linear(512, 512)
+        self.out = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = x.view(-1,28*28)
+        x = F.relu(self.hidden1(x), inplace=True) 
+        x = F.relu(self.hidden2(x), inplace=True)
+        x = self.out(x)
+
+        return F.log_softmax(x, dim=1)
+
+class MNIST_RNN(nn.Module):
+    def __init__(self):
+        super(MNIST_RNN, self).__init__()
+
+        self.rnn = nn.LSTM(         # if use nn.RNN(), it hardly learns
+            input_size=28,
+            hidden_size=64,         # rnn hidden unit
+            num_layers=1,           # number of rnn layer
+            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+
+        self.out = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = x.view(-1,28,28)
+        r_out, (h_n, h_c) = self.rnn(x, None)   
+
+        out = self.out(r_out[:, -1, :])
+        return out
 
 
-# 返回数据的信息：尺寸，通道，标签数量
-def get_model(dataset,model="CNN"):
 
-    if dataset =='MNIST':
-        width, height, dim, num_label = 28, 28, 1, 10
-    elif dataset =='CIFAR10':
-        width, height, dim, num_label = 32, 32, 3, 10
-        return CNNCIFAR(dim, width, height, num_label)
+# ----------------------  EMNIST  -------------------------
+
+class EMNIST_CNN(nn.Module):
+    def __init__(self):
+        super(EMNIST_CNN,self).__init__()
+
+        self.conv1 = nn.Sequential(        
+            nn.Conv2d(1,16,3,1,1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.conv2 = nn.Sequential(       
+            nn.Conv2d(16,32,3,1,1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.fc1 = nn.Linear(7*7*32,128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self,x):
+        out_conv1 = self.conv1(x)
+        out_conv2 = self.conv2(out_conv1)
+        output = out_conv2.view(-1,7*7*32)
+        output = F.relu(self.fc1(output))
+        output = self.fc2(output)
+        return F.log_softmax(output,dim=1)
+
+
+
+# ----------------------  CIFAR10  -------------------------
+
+class CIFAR10_VGG9(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFAR10_VGG9, self).__init__()
+        self.conv_layer = nn.Sequential(
+            # Conv Layer block 1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Conv Layer block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer block 3
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self._initialize_weights()
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(4096, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_layer(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_layer(x)
+        return F.log_softmax(x, dim=1)
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(0.5)
+                m.bias.data.zero_()
+
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(
+                inchannel,
+                outchannel,
+                kernel_size=3,
+                stride=stride,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(outchannel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(outchannel),
+        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    inchannel, outchannel, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(outchannel),
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+class ResNet(nn.Module):
+    def __init__(self, residual_block, num_classes=10):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(residual_block, 64, 2, stride=1)
+        self.layer2 = self.make_layer(residual_block, 128, 2, stride=2)
+        self.layer3 = self.make_layer(residual_block, 256, 2, stride=2)
+        self.layer4 = self.make_layer(residual_block, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)  # strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        # out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+def CIFAR10_ResNet18():
+    return ResNet(ResidualBlock)
+
+
+
+# ----------------------  CIFAR100  -------------------------
+
+def CIFAR100_ResNet18():
+    return ResNet(ResidualBlock,num_classes=100)
+
+
+
+
+def get_model(dataset,model_name="CNN",batch=None):
+    
+    if dataset == "MNIST":
+        if model_name == "CNN":
+            model = MNIST_CNN()
+        elif model_name == "MCLR":
+            model = MNIST_LR_Net()
+        elif model_name == "RNN":
+            model = MNIST_RNN()
+        else:
+            model = "break"
+
+    elif dataset == "EMNIST":
+        model = EMNIST_CNN()
+
+    elif dataset == "CIFAR10":
+        if model_name == "CNN":
+            model =  CIFAR10_VGG9()
+        else:
+            model =  CIFAR10_ResNet18()
+
     else:
-        width, height, dim, num_label = 28, 28, 1, 62
+        model = CIFAR100_ResNet18()
 
-    if model == "CNN":
-        m = CNN(dim, width, height, num_label)
-    elif model == "DNN":
-        m = DNN(dim, width, height, num_label)
-    else:
-        m = MCLR_Logistic(dim, width, height, num_label)
-
-    return m
+    return model
